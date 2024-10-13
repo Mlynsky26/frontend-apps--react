433f1b8571c853107fe3ecb103919093f2fc933b
Mateusz Moniowski <mlynski123@gmail.com>
2024-10-13 21:16:59 +0200

    Init

---
+++ src/module-data.js
@@ -0,0 +1,92 @@
+export const data = [
+{
+	id: "1",
+	name: "Magda",
+	eyeColor: "brown",
+	birthDate: "22/03/2001"
+},
+{
+	id: "2",
+	name: "Janusz",
+	eyeColor: "blue",
+	birthDate: "29/06/1980"
+},
+{
+	id: "3",
+	name: "Magda",
+	eyeColor: "brown",
+	birthDate: "19/03/2005"
+},
+{
+	id: "4",
+	name: "Janusz",
+	eyeColor: "brown",
+	birthDate: "25/03/1995"
+},
+{
+	id: "5",
+	name: "Jarek",
+	eyeColor: "green",
+	birthDate: "15/06/1989"
+},
+{
+	id: "6",
+	name: "Janusz",
+	eyeColor: "blue",
+	birthDate: "12/04/1983"
+},
+{
+	id: "7",
+	name: "Jarek",
+	eyeColor: "gray",
+	birthDate: "25/07/2000"
+},
+{
+	id: "8",
+	name: "Magda",
+	eyeColor: "yellow",
+	birthDate: "06/06/1977"
+},
+{
+	id: "9",
+	name: "Stanisław",
+	eyeColor: "green",
+	birthDate: "06/12/2001"
+},
+{
+	id: "10",
+	name: "Janusz",
+	eyeColor: "brown",
+	birthDate: "20/04/1979"
+},
+{
+	id: "11",
+	name: "Janusz",
+	eyeColor: "green",
+	birthDate: "06/05/1980"
+},
+{
+	id: "12",
+	name: "Jarek",
+	eyeColor: "blue",
+	birthDate: "26/11/1972"
+},
+{
+	id: "13",
+	name: "Kornelia",
+	eyeColor: "gray",
+	birthDate: "10/08/1986"
+},
+{
+	id: "14",
+	name: "Stanisław",
+	eyeColor: "brown",
+	birthDate: "22/12/1970"
+},
+{
+	id: "15",
+	name: "Magda",
+	eyeColor: "yellow",
+	birthDate: "06/10/1978"
+},
+];
